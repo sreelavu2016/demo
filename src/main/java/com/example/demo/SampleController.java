e6de0be6a23792e78091ff82a425e6a0e0e32aea
sreelavu2016 <sreenu.soft2012@gmail.com>
2020-11-16 12:24:30 +0530

    Update SampleController.java

--- src/main/java/com/example/demo/SampleController.java
+++ src/main/java/com/example/demo/SampleController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class SampleController {
